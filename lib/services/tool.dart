835fded0eb142f3062ba0fddf577c4a5f1e56102
DouDouMi00 <doudoumi00@qq.com>
2024-09-08 15:22:27 +0800

    refactor(tool): 使用flutter_emoji优化isStringAllEmojis函数
    重构isStringAllEmojis以使用EmojiParser，提高emoji检测的效率和准确性。通过引入flutter_emoji库，清理了之前的正则表达式实现，并简化了函数逻辑。

--- lib/services/tool.dart
+++ lib/services/tool.dart
@@ -1,12 +1,15 @@
+import 'package:flutter_emoji/flutter_emoji.dart';
+
+var parser = EmojiParser();
+
 bool isStringAllEmojis(String input) {
-  RegExp emojiRegex = RegExp(
-    r'[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}\u{2700}-\u{27BF}]',
-    unicode: true,
-  );
-
-  // 直接检查整个字符串是否完全由emoji组成，无需先清理字符串
-  bool isAllEmojis = emojiRegex.hasMatch(input) &&
-      emojiRegex.allMatches(input).length == input.runes.length;
-
-  return isAllEmojis;
+  if (input.isEmpty) {
+    return false;
+  }
+  for (var rune in input.runes) {
+    if (!parser.hasEmoji(String.fromCharCode(rune))) {
+      return false;
+    }
+  }
+  return true;
 }
